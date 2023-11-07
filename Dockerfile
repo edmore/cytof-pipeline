7beb4f3167868cc41cb6c375befe98a01ce2251a
Edmore Moyo <etmoyo@gmail.com>
2023-11-07 14:04:20 -0500

    updates to incorporate download from pennsieve

--- Dockerfile
+++ Dockerfile
@@ -5,22 +5,22 @@ WORKDIR /service
 RUN apt clean && apt-get update
 # install dependencies
 RUN apt-get -y install wget && apt-get -y install gnupg && apt-get -y install curl
-RUN wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add -
-RUN echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-RUN apt-get update
-# R program dependencies
-RUN apt-get install -y  libudunits2-dev libgdal-dev libgeos-dev libproj-dev && apt-get -y install libnlopt-dev && apt-get -y install pkg-config && apt-get -y install gdal-bin && apt-get install -y libgdal-dev
-# next flow dependencies
-RUN apt-get -y install temurin-17-jdk
+# RUN wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | apt-key add -
+# RUN echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+# RUN apt-get update
+# # R program dependencies
+# RUN apt-get install -y  libudunits2-dev libgdal-dev libgeos-dev libproj-dev && apt-get -y install libnlopt-dev && apt-get -y install pkg-config && apt-get -y install gdal-bin && apt-get install -y libgdal-dev
+# # next flow dependencies
+# RUN apt-get -y install temurin-17-jdk
 
-# install nextflow
-RUN wget -qO- https://get.nextflow.io | bash && chmod +x nextflow && cp ./nextflow /usr/local
-RUN apt-get -y install graphviz
+# # install nextflow
+# RUN wget -qO- https://get.nextflow.io | bash && chmod +x nextflow && cp ./nextflow /usr/local
+# RUN apt-get -y install graphviz
 
-ENV PATH="${PATH}:/usr/local/"
+# ENV PATH="${PATH}:/usr/local/"
 
-# cleanup
-RUN rm -f /service/nextflow
+# # cleanup
+# RUN rm -f /service/nextflow
 
 # set desired nextflow version
 # RUN export NXF_VER=23.04.1
@@ -39,29 +39,31 @@ COPY . ./
 
 RUN go build -o /service/main main.go
 
-# install devtools
-RUN apt-get -y install libcurl4-openssl-dev libfontconfig1-dev libxml2-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
-RUN apt install -y build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libgit2-dev cmake libglpk-dev
-RUN Rscript -e "install.packages(c('BH'), repos = 'https://cloud.r-project.org/', dependencies = TRUE)"
-RUN Rscript -e "install.packages(c('devtools'), repos = 'https://cloud.r-project.org/', dependencies = TRUE)"
+# # install devtools
+# RUN apt-get -y install libcurl4-openssl-dev libfontconfig1-dev libxml2-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+# RUN apt install -y build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev libgit2-dev cmake libglpk-dev
+# RUN Rscript -e "install.packages(c('BH'), repos = 'https://cloud.r-project.org/', dependencies = TRUE)"
+# RUN Rscript -e "install.packages(c('devtools'), repos = 'https://cloud.r-project.org/', dependencies = TRUE)"
 
-# install BiocManager
-RUN R --version
-RUN Rscript -e "install.packages('BiocManager')"
+# # install BiocManager
+# RUN R --version
+# RUN Rscript -e "install.packages('BiocManager')"
 
-# install I3HQC package
-RUN Rscript -e "install.packages(c('ggplot2', 'readxl', 'dplyr', 'RColorBrewer', 'viridis', 'cowplot', 'patchwork', 'tidyr', 'stringr', 'ggsci', 'magrittr', 'mblm', 'rstatix', 'psych', 'ggbeeswarm', 'umap', 'reshape2', 'pheatmap', 'plotly', 'spdep'), Ncpus = 10)" # for Cytof Report
-RUN Rscript -e "install.packages(c('ggpubr'), repos = 'https://cloud.r-project.org/', dependencies = TRUE)"
-RUN Rscript -e "install.packages(c('nloptr', 'lme4', 'pbkrtest', 'car', 'lubridate', 'gt', 'gtsave', 'chromate'), Ncpus = 10)"
-RUN Rscript -e "BiocManager::install('RProtoBufLib')"
-RUN Rscript -e "BiocManager::install('cytolib')"
-RUN Rscript -e "BiocManager::install('flowCore')"
-RUN Rscript -e "BiocManager::install('FlowSOM')"
-RUN Rscript -e "install.packages(c('tidyverse'), Ncpus = 10)"
-RUN Rscript -e "require(devtools);library(devtools);devtools::install_github('ianmoran11/mmtable2');devtools::install_local('./I3HQC.zip')"
+# # install I3HQC package
+# RUN Rscript -e "install.packages(c('ggplot2', 'readxl', 'dplyr', 'RColorBrewer', 'viridis', 'cowplot', 'patchwork', 'tidyr', 'stringr', 'ggsci', 'magrittr', 'mblm', 'rstatix', 'psych', 'ggbeeswarm', 'umap', 'reshape2', 'pheatmap', 'plotly', 'spdep'), Ncpus = 10)" # for Cytof Report
+# RUN Rscript -e "install.packages(c('ggpubr'), repos = 'https://cloud.r-project.org/', dependencies = TRUE)"
+# RUN Rscript -e "install.packages(c('nloptr', 'lme4', 'pbkrtest', 'car', 'lubridate', 'gt', 'gtsave', 'chromate'), Ncpus = 10)"
+# RUN Rscript -e "BiocManager::install('RProtoBufLib')"
+# RUN Rscript -e "BiocManager::install('cytolib')"
+# RUN Rscript -e "BiocManager::install('flowCore')"
+# RUN Rscript -e "BiocManager::install('FlowSOM')"
+# RUN Rscript -e "install.packages(c('tidyverse'), Ncpus = 10)"
+# RUN Rscript -e "require(devtools);library(devtools);devtools::install_github('ianmoran11/mmtable2');devtools::install_local('./I3HQC.zip')"
 
 # install aws-lambda-rie
 # RUN curl -Lo aws-lambda-rie https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie \
 # && chmod +x aws-lambda-rie && mv aws-lambda-rie /usr/local/bin/aws-lambda-rie
+
+RUN mkdir -p data
 
 ENTRYPOINT [ "/service/main" ]
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 
 	"log/slog"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -14,32 +26,242 @@ func main() {
 	slog.SetDefault(logger)
 
 	integrationID := os.Getenv("INTEGRATION_ID")
-	inputDir := "/service/data"
-	outDir := "/mnt"
-
-	if os.Getenv("INPUT_DIR") != "" {
-		inputDir = os.Getenv("INPUT_DIR")
+	baseDir := os.Getenv("BASE_DIR")
+	if integrationID == "" {
+		id := uuid.New()
+		integrationID = id.String()
 	}
-	if os.Getenv("OUT_DIR") != "" {
-		outDir = os.Getenv("OUT_DIR")
+	if baseDir == "" {
+		baseDir = "/mnt/efs"
 	}
 
-	// TODO: create input and output directories -> /mnt/input<integrationID> and /mnt/output<integrationID>
+	logger.Info(integrationID)
+	// create subdirectories
+	err := os.Chdir(baseDir)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
-	// run pipeline
-	cmd := exec.Command("nextflow", "run", "/service/main.nf", "-ansi-log", "false", "--integration", integrationID, "--inputDir", inputDir, "--outDir", outDir)
-	cmd.Dir = "/service"
+	err = os.MkdirAll(fmt.Sprintf("input/%s", integrationID), 0755)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	err = os.MkdirAll("output", 0777)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	err = os.Chown("output", 1000, 1000)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	err = os.MkdirAll(fmt.Sprintf("output/%s", integrationID), 0777)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	err = os.Chown(fmt.Sprintf("output/%s", integrationID), 1000, 1000)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// get input files
+	sessionToken := os.Getenv("SESSION_TOKEN")
+	apiHost := os.Getenv("PENNSIEVE_API_HOST")
+	packages := getPackageIds()
+	manifest, err := getPresignedUrls(apiHost, packages, sessionToken)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(manifest))
+	var payload Manifest
+	if err := json.Unmarshal(manifest, &payload); err != nil {
+		logger.ErrorContext(context.Background(), err.Error())
+	}
+	fmt.Println(payload.Data)
+	for _, d := range payload.Data {
+		cmd := exec.Command("wget", "-O", d.FileName, d.Url)
+		cmd.Dir = fmt.Sprintf("input/%s", integrationID)
+		var out strings.Builder
+		var stderr strings.Builder
+		cmd.Stdout = &out
+		cmd.Stderr = &stderr
+		if err := cmd.Run(); err != nil {
+			logger.Error(err.Error(),
+				slog.String("error", stderr.String()))
+		}
+	}
+
+	inputFileName := fmt.Sprintf("input/%s/test.txt", integrationID)
+	// process input file
+	outputFileName := fmt.Sprintf("output/%s/test.txt", integrationID)
+	input, err := os.ReadFile(inputFileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, "YEH") {
+			lines[i] = "LOL"
+		}
+	}
+	output := strings.Join(lines, "\n")
+
+	// write contents to output dir
+	err = os.WriteFile(outputFileName, []byte(output), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmd := exec.Command("cat", inputFileName)
+	cmd.Dir = baseDir
 	var out strings.Builder
 	var stderr strings.Builder
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	if err := cmd.Run(); err != nil {
-		logger.Error(err.Error(),
-			slog.String("error", stderr.String()))
+		log.Println(err.Error())
+		log.Println(stderr.String())
+	}
+	log.Println("cat output", out.String())
+
+	// invoke Post processing
+	// cfg, err := config.LoadDefaultConfig(context.Background())
+	// if err != nil {
+	// 	log.Fatalf("LoadDefaultConfig: %v\n", err)
+	// }
+	// client := lambda.NewFromConfig(cfg)
+	// _, err = client.Invoke(context.Background(),
+	// 	&lambda.InvokeInput{
+	// 		FunctionName: aws.String(os.Getenv("POST_PROCESSOR_INVOKE_ARN")),
+	// 	})
+
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	TaskDefinitionArn := os.Getenv("TASK_DEFINITION_NAME_POST")
+	subIdStr := os.Getenv("SUBNET_IDS")
+	SubNetIds := strings.Split(subIdStr, ",")
+	cluster := os.Getenv("CLUSTER_NAME")
+	SecurityGroup := os.Getenv("SECURITY_GROUP_ID")
+	TaskDefContainerName := os.Getenv("CONTAINER_NAME_POST")
+	apiKey := os.Getenv("PENNSIEVE_API_KEY")
+	apiSecret := os.Getenv("PENNSIEVE_API_SECRET")
+
+	agentHome := os.Getenv("PENNSIEVE_AGENT_HOME")
+
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatalf("LoadDefaultConfig: %v\n", err)
 	}
 
-	logger.Info("pipeline output",
-		slog.String("output", out.String()))
+	client := ecs.NewFromConfig(cfg)
+	log.Println("Initiating post processing Task.")
+	apiKeyKey := "PENNSIEVE_API_KEY"
+	apiSecretKey := "PENNSIEVE_API_SECRET"
+	apihostKey := "PENNSIEVE_API_HOST"
+	agentHomeKey := "PENNSIEVE_AGENT_HOME"
+	integrationIDKey := "INTEGRATION_ID"
+
+	runTaskIn := &ecs.RunTaskInput{
+		TaskDefinition: aws.String(TaskDefinitionArn),
+		Cluster:        aws.String(cluster),
+		NetworkConfiguration: &types.NetworkConfiguration{
+			AwsvpcConfiguration: &types.AwsVpcConfiguration{
+				Subnets:        SubNetIds,
+				SecurityGroups: []string{SecurityGroup},
+				AssignPublicIp: types.AssignPublicIpEnabled,
+			},
+		},
+		Overrides: &types.TaskOverride{
+			ContainerOverrides: []types.ContainerOverride{
+				{
+					Name: &TaskDefContainerName,
+					Environment: []types.KeyValuePair{
+						{
+							Name:  &apiKeyKey,
+							Value: &apiKey,
+						},
+						{
+							Name:  &apiSecretKey,
+							Value: &apiSecret,
+						},
+						{
+							Name:  &apihostKey,
+							Value: &apiHost,
+						},
+						{
+							Name:  &agentHomeKey,
+							Value: &agentHome,
+						},
+						{
+							Name:  &integrationIDKey,
+							Value: &integrationID,
+						},
+					},
+				},
+			},
+		},
+		LaunchType: types.LaunchTypeFargate,
+	}
+
+	_, err = client.RunTask(context.Background(), runTaskIn)
+	if err != nil {
+		log.Fatalf("error running task: %v\n", err)
+	}
 
 	logger.Info("Processing complete")
+}
+
+func getPackageIds() Packages {
+	return Packages{
+		NodeIds: []string{"N:package:10024142-6f36-4104-b311-d281cb0cadcd"},
+	}
+}
+
+type Packages struct {
+	NodeIds []string `json:"nodeIds"`
+}
+
+type Manifest struct {
+	Data []ManifestData `json:"data"`
+}
+
+type ManifestData struct {
+	NodeId   string   `json:"nodeId"`
+	FileName string   `json:"fileName"`
+	Path     []string `json:"path"`
+	Url      string   `json:"url"`
+}
+
+func getPresignedUrls(apiHost string, packages Packages, sessionToken string) ([]byte, error) {
+	url := fmt.Sprintf("%s/packages/download-manifest?api_key=%s", apiHost, sessionToken)
+	b, err := json.Marshal(packages)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(b))
+
+	payload := strings.NewReader(string(b))
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("accept", "*/*")
+	req.Header.Add("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	return body, nil
 }
